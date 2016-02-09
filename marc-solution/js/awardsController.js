f5ff79cecf5840e13aa619108b01c05f331be91f
marcwright <marc.wright@gmail.com>
2016-02-09 15:43:40 -0500

    adding solutions

---
+++ marc-solution/js/awardsController.js
@@ -0,0 +1,22 @@
+// Adding ngAnimate module
+angular.module('Awwward', ['ngAnimate'])
+  .controller('awardsController', AwardsController);
+
+function AwardsController(){
+  this.newWinner = {};
+  this.winners = [
+    {name: "Winston Inner"},
+    {name: "Justhe Besse"},
+    {name: "Sophia Close"},
+    {name: "Chappy Goluckee"},
+    {name: "Barry Wheldon"},
+    {name: "Curtis Rusty"},
+    {name: "Noah Ward" }
+  ];
+  this.createNewWinner = createNewWinner;
+
+  function createNewWinner(){
+    this.winners.push(this.newWinner);
+    this.newWinner = {};
+  }
+}
